821b1b116d11c2cbd9680c0abd1c03506c78c76e
Ahmed Dev <ahmedshuaib46@gmail.com>
2020-03-18 13:36:29 +0600

    json parser fixed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,9 @@
 #include "network/networkmanager.h"
 #include <QMessageBox>
 #include <QDebug>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -30,7 +33,26 @@ bool MainWindow::validation()
 
 void MainWindow::slots_response_receive(const QByteArray &array)
 {
-    qInfo() << array;
+    QStringList propertyNames;
+    QStringList propertyKeys;
+    QString strReplay = (QString)array;
+    qInfo() << strReplay;
+    QJsonDocument json_response = QJsonDocument::fromJson(strReplay.toUtf8());
+    QJsonObject json_object = json_response.object(); //make object
+
+
+    auto obj = json_object["form"].toObject();
+
+    qInfo() << "Foo : " <<  obj["foo1"].toString();
+    qInfo() << "Foo2: " <<  obj["foo2"].toString();
+
+//    QJsonArray json_array = json_object["properties"].toArray(); //make an array for []
+
+
+    //for json array
+//    foreach (const QJsonValue & value, json_array) {
+//        QJsonObject obj = value.toObject();
+//    }
 }
 
 void MainWindow::on_submit_form_clicked()
@@ -39,10 +61,9 @@ void MainWindow::on_submit_form_clicked()
         QMessageBox::information(this, "Incorrect information", "Username or password field are empty?");
     }
 
-    controller->set_url("https://google.com");
-//    controller->manager()->set_post_fields("foo1=bar1&foo2=bar2");
+    controller->set_url("https://postman-echo.com/post");
+    controller->manager()->set_post_fields("foo1=bar1&foo2=bar2");
     controller->manager()->skip_ssl();
     controller->submit();
-    qInfo() << "Resonse: " << controller->manager()->Response();
 
 }
